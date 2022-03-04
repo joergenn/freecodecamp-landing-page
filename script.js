d756ef3bb2dfee6ede88e8060552ed7d9afed0d9
joergenn <andrian1993a@gmail.com>
2022-03-04 17:46:15 +0200

    added hamburger menu

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.ham-menu');
+const menu = document.querySelector('.nav-bar');
+
+button.addEventListener('click', () => {
+    if(menu.style.display == 'flex'){
+        menu.style.display = 'none';
+    }
+    else{
+        menu.style.display = 'flex';
+    }   
+})
