649a1c837d6f8fe1b462545b0eb3d9893a00eb9b
joergenn <andrian1993a@gmail.com>
2022-03-05 23:18:52 +0200

    added items, json file reading

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-const button = document.querySelector('.ham-menu');
-const menu = document.querySelector('.nav-bar');
+const button = document.querySelector('.ham-menu'),
+      menu = document.querySelector('.nav-bar'),
+      goods = document.querySelector('.goods'),
+      showMoreBtn = document.querySelector('.show-more');
+
+let data;
+fetch("./data.json")
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(jsonData){
+        data = jsonData;
+    });
+
+function append(){
+    let newData = data.slice(-4);
+    for(let i = 0; i < 4; i++){
+       data.pop();
+    }
+
+    goods.insertAdjacentHTML('beforeEnd', `${newData.map(function(item){
+        return `
+                <div class="item">
+                    <img src="${item.image}" alt="Trombone" class="image">
+                    <div class="description">
+                        <h3>${item.name}</h3>
+                        <p>${item.description}</p>
+                    </div>
+                    <div class="buy-section">
+                        <span class="price">${item.price}$</span>
+                        <button class="buy-btn"><a href="#">BUY IT</a></button>
+                    </div>
+                </div>
+        `
+    }).join('')}`)   
+
+    if(data.length == 0){
+        showMoreBtn.style.display = 'none';
+    }
+}
+
+showMoreBtn.addEventListener('click', append);
+
+
 
 button.addEventListener('click', () => {
     if(menu.style.display == 'flex'){
@@ -9,3 +51,4 @@ button.addEventListener('click', () => {
         menu.style.display = 'flex';
     }   
 })
+
